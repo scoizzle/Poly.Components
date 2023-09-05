4282f9d67e8dd36e17a2bcc540ec3e407949e660
Scot Murphy <scot@scoizzle.com>
2023-09-04 23:12:29 -0500

    Initial Commit

---
+++ src/Poly.Components/Load/Load.StateMachine.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Components;
+using Stateless;
+
+namespace Poly.Components;
+
+public partial class Load<T> : ComponentBase, IAsyncDisposable {
+    static readonly StateMachine<LoadState, LoadTrigger>.TriggerWithParameters<T?> EndLoadingTrigger = new(LoadTrigger.EndLoading);
+    static readonly StateMachine<LoadState, LoadTrigger>.TriggerWithParameters<Exception> LoadErrorTrigger = new(LoadTrigger.LoadError);
+
+    internal enum LoadState {
+        Init,
+        Loading,
+        Refreshing,
+        Loaded,
+        Stale,
+        Error,
+        Disposed
+    }
+
+    internal enum LoadTrigger {
+        BeginLoading,
+        CancelLoading,
+        LoadError,
+        EndLoading,
+        Dispose
+    }
+
+    private void ConfigureStateMachine() {
+        stateMachine.Configure(LoadState.Init)
+            .OnExit(() => currentContent = Loading)
+            .Permit(LoadTrigger.BeginLoading, LoadState.Loading)
+            .Permit(LoadTrigger.Dispose, LoadState.Disposed);
+
+        stateMachine.Configure(LoadState.Loading)
+            .OnEntry(OnBeginLoading)
+            .PermitDynamic(EndLoadingTrigger, _ => LoadState.Loaded)
+            .PermitDynamic(LoadErrorTrigger, _ => LoadState.Error)
+            .Permit(LoadTrigger.CancelLoading, LoadState.Error)
+            .Permit(LoadTrigger.Dispose, LoadState.Disposed)
+            .Ignore(LoadTrigger.BeginLoading);
+
+        stateMachine.Configure(LoadState.Loaded)
+            .OnEntryFrom(EndLoadingTrigger, OnEndLoading)
+            .Permit(LoadTrigger.BeginLoading, LoadState.Refreshing)
+            .Permit(LoadTrigger.Dispose, LoadState.Disposed)
+            .Ignore(LoadTrigger.CancelLoading);
+
+        stateMachine.Configure(LoadState.Refreshing)
+            .OnEntry(OnBeginLoading)
+            .PermitDynamic(EndLoadingTrigger, _ => LoadState.Loaded)
+            .PermitDynamic(LoadErrorTrigger, _ => LoadState.Stale)
+            .Permit(LoadTrigger.CancelLoading, LoadState.Stale)
+            .Permit(LoadTrigger.Dispose, LoadState.Disposed)
+            .Ignore(LoadTrigger.BeginLoading);
+
+        stateMachine.Configure(LoadState.Stale)
+            .OnEntryFrom(LoadErrorTrigger, OnLoadStale)
+            .Permit(LoadTrigger.BeginLoading, LoadState.Refreshing)
+            .Permit(LoadTrigger.EndLoading, LoadState.Loaded)
+            .Permit(LoadTrigger.Dispose, LoadState.Disposed)
+            .Ignore(LoadTrigger.CancelLoading);
+
+        stateMachine.Configure(LoadState.Error)
+            .OnEntryFrom(LoadErrorTrigger, OnLoadError)
+            .Permit(LoadTrigger.BeginLoading, LoadState.Loading)
+            .Permit(LoadTrigger.Dispose, LoadState.Disposed);
+
+        stateMachine.Configure(LoadState.Disposed)
+            .PermitReentry(LoadTrigger.Dispose)
+            .Ignore(LoadTrigger.CancelLoading);
+    }
+}
---
+++ src/Poly.Components/Load/Load.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Stateless;
+
+namespace Poly.Components;
+
+public partial class Load<T> : ComponentBase, IAsyncDisposable
+{
+    [Parameter, EditorRequired]
+    public Func<CancellationToken, Task<T?>>? From { get; set; }
+
+    [Parameter]
+    public RenderFragment<T?>? Loaded { get; set; }
+
+    [Parameter] 
+    public T? Data { get; set; }
+
+    [Parameter] 
+    public EventCallback<T?> DataChanged { get; set; }
+
+    [Parameter]
+    public TimeSpan? Every { get; set; }
+
+    [Parameter] 
+    public object? ReloadOnChange { get; set; }
+
+    [Parameter]
+    public bool StaleDataIsAnError { get; set; }
+
+    [Parameter] 
+    public RenderFragment Loading { get; set; } = builder => builder.AddMarkupContent(0, "Loading...");
+
+    [Parameter] 
+    public RenderFragment<Exception?> Error { get; set; } = _ => builder => builder.AddMarkupContent(0, "Error...");
+
+    private object? currentReloadSentinelValue;
+    private PeriodicTimer? _timer;
+    private RenderFragment? currentContent;
+    private StateMachine<LoadState, LoadTrigger> stateMachine = new(LoadState.Init);
+    private CancellationTokenSource cancellationTokenSource = new();
+
+    public Load() => ConfigureStateMachine();
+
+    private async void OnBeginLoading() {
+        if (From is null)
+            throw new ArgumentNullException(nameof(From));
+
+        if (cancellationTokenSource.IsCancellationRequested)
+            cancellationTokenSource = new();
+
+        try {
+            var result = await From(cancellationTokenSource.Token);
+
+            stateMachine.Fire(EndLoadingTrigger, result);
+        }
+        catch (TaskCanceledException) {
+            stateMachine.Fire(LoadTrigger.CancelLoading);
+        }
+        catch (Exception error) {
+            stateMachine.Fire(LoadErrorTrigger, error);
+        }
+    }
+
+    private async void OnEndLoading(T? data) {
+        if (ReferenceEquals(Data, data))
+            return;
+
+        if (Equals(Data, data))
+            return;
+
+        if (Loaded is not null)
+            currentContent = Loaded!(data);
+
+        if (DataChanged.HasDelegate)
+            await DataChanged.InvokeAsync(data);
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private async void OnLoadError(Exception error) {
+        currentContent = Error?.Invoke(error);
+
+        await InvokeAsync(StateHasChanged);
+    }
+
+    private void OnLoadStale(Exception error) {
+        if (!StaleDataIsAnError)
+            return;
+
+        OnLoadError(error);
+    }
+
+    private async void InitializeTimerLoop(TimeSpan? period) {
+        _timer?.Dispose();
+
+        if (!period.HasValue || period.Value <= TimeSpan.Zero)
+            return;
+
+        _timer = new(period.Value);
+
+        try {
+            while (await _timer.WaitForNextTickAsync(cancellationTokenSource.Token)) {
+                stateMachine.Fire(LoadTrigger.BeginLoading);
+            }
+        }
+        catch (TaskCanceledException) { }
+        catch (OperationCanceledException) { }
+    }
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+        => currentContent?.Invoke(builder);
+
+    protected override void OnInitialized() 
+        => stateMachine.Fire(LoadTrigger.BeginLoading);
+
+    public override async Task SetParametersAsync(ParameterView parameters) {
+        var previousPeriod = Every;
+
+        await base.SetParametersAsync(parameters);
+
+        if (Every == previousPeriod)
+            return;
+
+        InitializeTimerLoop(Every);
+    }
+
+    protected override void OnParametersSet() {
+        if (ReferenceEquals(ReloadOnChange, currentReloadSentinelValue))
+            return;
+
+        if (Equals(ReloadOnChange, currentReloadSentinelValue))
+            return;
+
+        currentReloadSentinelValue = ReloadOnChange;
+
+        stateMachine.Fire(LoadTrigger.BeginLoading);
+    }
+
+    public async ValueTask DisposeAsync() {
+        await stateMachine.FireAsync(LoadTrigger.Dispose);
+
+        cancellationTokenSource?.Cancel();
+    }
+    
+    public void Reload() {
+        stateMachine.Fire(LoadTrigger.BeginLoading);
+    }
+}
